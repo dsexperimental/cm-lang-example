e16990458817449136d6c70d5bd2bee2ce746dce
Dave Sutter <sutter@intransix.com>
2023-02-11 16:06:25 -0800

    added a state tracker to track when we are in different sets of braces/barckets/parens, trying to manage when new line is an expression delimiter or not.

---
+++ src/tokens.js
@@ -0,0 +1,55 @@
+/* Hand-written tokenizers for R tokens that can't be
+   expressed by lezer's built-in tokenizer. */
+
+import {ContextTracker} from "@lezer/lr"
+
+import { openBrace, closeBrace, openParen, closeParen,
+         openBrack, closeBrack, openDBrack, closeDBrack,
+         hnl, snl } from "./parser.terms"
+
+const opens = new Set([openBrace,openParen,openBrack,openDBrack])
+const closes  = new Set([closeBrace,closeParen,closeBrack,closeDBrack])
+
+class ContextState {
+  constructor(parent, delimiter) {
+    this.parent = parent
+    this.delimiter = delimiter
+    this.hardDelim = (delimiter == openBrace)
+  }
+
+  canPopWith(delimiter) {
+    if(parent == null) return false
+    switch(this.delimiter) {
+      case openBrace: return delimiter == closeBrace
+      case openParen: return delimiter == closeParen
+      case openBrack: return delimiter == closeBrack
+      case openDBrack: return delimiter == closeDBrack
+      default: false
+    }
+  }
+}
+
+export const trackDelims = new ContextTracker({
+  start: new ContextState(null,openBrace), //initial state, Script, is like a block
+  shift(context, term) {
+    if(opens.has(term)) {
+      return new ContextState(context,term)
+    }
+    else if( closes.has(term) && context.canPopWith(term) ) {
+        return context.parent
+    }
+    else {
+      return context
+    }
+  },
+  strict: false
+})
+
+export const replaceNewline = function(token,stack) {
+  if(stack.context.hardDelim) {
+    return hnl
+  }
+  else {
+    return snl
+  }
+}
