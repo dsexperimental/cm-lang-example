0e4cd90fe87c6d02e8c5b221c0227709dca9ca1c
Dave Sutter <sutter@intransix.com>
2023-02-12 20:08:50 -0800

    more grammar refinements

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import {parser} from "./syntax.grammar"
-import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
+import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent, indentRange} from "@codemirror/language"
 import {styleTags, tags as t} from "@lezer/highlight"
 
 export const EXAMPLELanguage = LRLanguage.define({
@@ -12,23 +12,33 @@ export const EXAMPLELanguage = LRLanguage.define({
         Block: foldInside
       }),
       styleTags({
-        Identifier: t.variableName,
+        "if else repeat while function for in next break": t.controlKeyword,
+        "NULL NA NA_integer_ NA_real_ NA_complex_ NA_Character_": t.null,
+        "Inf NaN": t.float,
         Numeric: t.float,
         Integer: t.integer,
         Complex: t.float,
         Logical: t.bool,
         Character: t.string,
         Comment: t.lineComment,
-        "function": t.lineComment,
-        "( )": t.paren
+        Identifier: t.variableName,
+        "[ ]  [[  ]]": t.bracket,
+        ArithOp: t.arithmeticOperator,
+        CompOp: t.compareOperator,
+        LogicOp: t.logicOperator,
+        AssignOp: t.definitionOperator,
+        GenOp: t.operator
       })
     ]
   }),
   languageData: {
-    commentTokens: {line: "#"}
+    commentTokens: {line: "#"},
+    closeBrackets: {brackets: ["(", "[", "[[", "{", "'", '"', "`"]}
   }
 })
 
 export function EXAMPLE() {
   return new LanguageSupport(EXAMPLELanguage)
 }
+
+
